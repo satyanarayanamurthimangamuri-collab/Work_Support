7e3bbdd1cf8077da5f5ea717bc854dbeadbdfca0
satyanarayana <satyanarayana@example.com>
2026-08-19 22:31:20 +0530

    Fix GitHub Pages asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
-    base: process.env.NODE_ENV === "production" ? "/Work_Support/" : "/",
+    base: "./",
     plugins: [react()],
 });
